1880ecd70e18ae4978ec59ffedfd8fab5f654965
DallinS <dallinstephens1@gmail.com>
2024-02-06 02:40:46 -0700

    Chamber Site

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,16 @@
+// References on how to generate the year:
+// https://byui-cse.github.io/wdd230-course/lesson01/applied-javascript.html
+// https://codepen.io/blazzard-jason/pen/LYrqxGJ
+// https://codepen.io/DallinS/pen/OJqbrZL
+const copyrightYear = {
+    year: "numeric"
+};
+
+document.querySelector('footer #year').textContent = new Date().toLocaleDateString("en-US", copyrightYear);
+
+// References to show the date and time the document was last modified
+// https://www.tutorialspoint.com/How-to-show-the-date-and-time-the-document-was-last-modified-with-JavaScript
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/lastModified
+let lastModificationDate = document.lastModified;
+
+document.querySelector('#lastModified').innerHTML = `Last Modification: ${lastModificationDate}`;
