ee009a7e5479021a24b1abee0066756f621395b4
DallinS <dallinstephens1@gmail.com>
2024-03-06 00:20:49 -0700

    Updated Chamber Home Page: removed img inline styling, added windchill calculator, and page load less than 500kB

--- chamber/scripts/wind-chill.js
+++ chamber/scripts/wind-chill.js
@@ -1,12 +1,26 @@
-const airTemperature = document.querySelector('#temperature').textContent;
-const windSpeed = document.querySelector('#windSpeed').textContent;
+const airTemperature = document.querySelector('#temperature');
+const windSpeed = document.querySelector('#windSpeed');
+
+// Reference for addEventListener change event: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
+airTemperature.addEventListener("change", CalculateWindChillIndex);
+windSpeed.addEventListener("change", CalculateWindChillIndex);
 
 // Reference for wind chill formula: https://byui-cse.github.io/wdd230-course/lesson06/aa-chamber-home-06.html
 // Reference for JavaScript operators: https://www.w3schools.com/js/js_operators.asp
 // References for wind chill examples: 
 // https://sciencing.com/types-oldfashioned-weather-instruments-6849423.html
 // https://www.weather.gov/media/ajk/brochures/Wind_Chill_Temperature_Index.pdf
-let windChillIndex = 35.74 + 0.6215 * airTemperature - 35.75 * windSpeed ** 0.16 + 0.4275 * airTemperature * windSpeed ** 0.16;
+function CalculateWindChillIndex() {
+    console.log(airTemperature.value);
+    console.log(windSpeed.value);
+    windChillIndex = 35.74 + 0.6215 * airTemperature.value - 35.75 * windSpeed.value ** 0.16 + 0.4275 * airTemperature.value * windSpeed.value ** 0.16;
 
-// Reference on how to round number: https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
-document.querySelector('#windChill').textContent = Math.round(windChillIndex);
+    if (airTemperature.value == "" || windSpeed.value == "") {
+        document.querySelector('#windChill').value = "N/A";
+    }
+    else {
+        // Reference on how to round number: https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+    document.querySelector('#windChill').value = Math.round(windChillIndex);
+    }  
+}
+
