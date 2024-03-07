44741b80f8611a66d5ddbdd2f336d353afda1316
DallinS <dallinstephens1@gmail.com>
2024-03-07 08:32:35 -0700

    Lesson 09 Fetch API Prophets Assignment

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -6,33 +6,33 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 
 const cards = document.querySelector('#cards');
 
-async function getProphetData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    // temporary testing of data retrieval
-    console.table(data.prophets); 
-}
-
-getProphetData();
-
-// let results = null;
-
 // async function getProphetData(url) {
 //     const response = await fetch(url);
-//     // check to see if the fetch was successful
-//     if (response.ok) {
-//         // The API will send us JSON...but we have to convert the
-//         // response before we can use it.
-//         // .json() also returns a promise...so we await it as well.
-//         const data = await response.json();
-//         doStuff(data);
-//     }
+//     const data = await response.json();
+//     // temporary testing of data retrieval
+//     console.table(data.prophets); 
 // }
 
-// function doStuff(data) {
-//     results = data;
-//     console.log("first: ", results);
-// }
+// getProphetData();
 
-// getProphetData(url);
-// console.log("second: ", results);
+let results = null;
+
+async function getProphetData(url) {
+    const response = await fetch(url);
+    // check to see if the fetch was successful
+    if (response.ok) {
+        // The API will send us JSON...but we have to convert the
+        // response before we can use it.
+        // .json() also returns a promise...so we await it as well.
+        const data = await response.json();
+        doStuff(data);
+    }
+}
+
+function doStuff(data) {
+    results = data;
+    console.log("first: ", results);
+}
+
+getProphetData(url);
+console.log("second: ", results);
