7e5546d3645cc562a462e8dffd56797022cc9973
DallinS <dallinstephens1@gmail.com>
2024-03-07 18:48:34 -0700

    JSON Menu Assignment

---
+++ scripts/links.js
@@ -0,0 +1,11 @@
+const baseURL = "https://dallinstephens.github.io/wdd230/";
+
+const linksURL = "https://dallinstephens.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+}
+
+getLinks();
