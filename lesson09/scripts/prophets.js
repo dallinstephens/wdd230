9ce388e462bdd7bfee590d28b70cbcabc28b6ff6
DallinS <dallinstephens1@gmail.com>
2024-03-09 13:14:27 -0700

    Lesson 9 Prophets JSON Activity

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -6,33 +6,73 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 
 const cards = document.querySelector('#cards');
 
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // temporary testing of data retrieval
+    // console.table(data.prophets);
+    // need to reference the prophets array of the JSON data object, not just the JSON data object
+    displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        // Create elmements for each card
+        // Reference for createElement(): https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+        let card = document.createElement("section");
+        let fullName = document.createElement("h2");
+        let portrait = document.createElement("img");
+        let birthDate = document.createElement("div");
+        let birthPlace = document.createElement("div");
+
+        // Build the full name
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        // Build birth date
+        birthDate.innerHTML = `Date of Birth: ${prophet.birthdate}`;
+
+        // Build birth place
+        birthPlace.innerHTML = `Place of Birth: ${prophet.birthplace}`;
+
+        // Build the image portrait
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("height", "440");
+
+        // Add the full name heading and image elements to the section element named card
+        // Reference for appendChild(): https://developer.mozilla.org/en-US/docs/Web/API/Element/append
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        // Append the section nameed card to the id of cards which we named cards
+        cards.appendChild(card);
+    });
+}
+
+// let results = null;
+
 // async function getProphetData(url) {
 //     const response = await fetch(url);
-//     const data = await response.json();
-//     // temporary testing of data retrieval
-//     console.table(data.prophets); 
+//     // check to see if the fetch was successful
+//     if (response.ok) {
+//         // The API will send us JSON...but we have to convert the
+//         // response before we can use it.
+//         // .json() also returns a promise...so we await it as well.
+//         const data = await response.json();
+//         doStuff(data);
+//     }
 // }
 
-// getProphetData();
+// function doStuff(data) {
+//     results = data;
+//     console.log("first: ", results);
+// }
 
-let results = null;
-
-async function getProphetData(url) {
-    const response = await fetch(url);
-    // check to see if the fetch was successful
-    if (response.ok) {
-        // The API will send us JSON...but we have to convert the
-        // response before we can use it.
-        // .json() also returns a promise...so we await it as well.
-        const data = await response.json();
-        doStuff(data);
-    }
-}
-
-function doStuff(data) {
-    results = data;
-    console.log("first: ", results);
-}
-
-getProphetData(url);
-console.log("second: ", results);
+// getProphetData(url);
+// console.log("second: ", results);
--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,7 @@
+// References used for help with this assignment:
+// https://byui-cse.github.io/wdd230-course/lesson09/aa-json.html
+// https://byui-cse.github.io/wdd230-course/lesson09/fetch.html
+
 const baseURL = "https://dallinstephens.github.io/wdd230/";
 
 const linksURL = "https://dallinstephens.github.io/wdd230/data/links.json";
@@ -5,7 +9,14 @@ const linksURL = "https://dallinstephens.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+    displayLinks(data.lessons);
 }
 
 getLinks();
+
+const displayLinks = (lessons) => {
+    lessons.forEach((lesson)) => {
+
+    });
+}
